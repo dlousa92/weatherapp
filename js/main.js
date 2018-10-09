29184ba45b7eb3b95a8bdd10c98f1d6daf6a8420
Daniel Lousa <dlousa92@gmail.com>
2018-10-09 15:41:47 -0400

    use fetch to set up query

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 let apiKey = "d2bef6ba1a012d7aebba54cedd1e5859"
+let units = "imperial"
+let searchMethod
+
+function searchWeather(term) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}`)
+}
