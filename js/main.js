b3e74155eaea939b4bfebebc07b7739d60dff711
Daniel Lousa <dlousa92@gmail.com>
2018-10-09 15:49:42 -0400

    turn result into json

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 let apiKey = "d2bef6ba1a012d7aebba54cedd1e5859"
 let units = "imperial"
-let searchMethod
+let searchMethod = "zip"
 
 function searchWeather(term) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${term}&APPID=${apiKey}&units=${units}`)
+        .then(result => {
+            return result.json()
+        }).then(result => {
+            init(result)
+        })
+}
+
+function init(res) {
+    console.log(res)
 }
