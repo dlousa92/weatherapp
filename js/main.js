f7f0fd22abda761e9dc9a19e704cd5c7e5342c6d
Daniel Lousa <dlousa92@gmail.com>
2018-10-09 22:26:32 -0400

    determine if search method is zip code or city name, flesh out search

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-let apiKey = "d2bef6ba1a012d7aebba54cedd1e5859"
+const apiKey = "d2bef6ba1a012d7aebba54cedd1e5859"
 let units = "imperial"
 let searchMethod = "zip"
 
-function searchWeather(term) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${term}&APPID=${apiKey}&units=${units}`)
+// sets whether the user is searching with zip code or city name
+function getSearchMethod (searchTerm) {
+    if (searchTerm.length === 5 && Number.parseInt(searchTerm) === searchTerm) {
+        searchMethod = "zip"
+    } else {
+        searchMethod = "q"
+    }
+}
+
+// executes the query
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm)
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`)
         .then(result => {
             return result.json()
         }).then(result => {
@@ -11,15 +23,19 @@ function searchWeather(term) {
         })
 }
 
+// populates all fields in our html file with the res
 function init(res) {
     console.log(res)
 }
 
-// selectors
+// selector functions
 
-let button =document.getElementById('searchButton')
+let button = document.getElementById("searchButton")
+
 button.addEventListener("click", () => {
-    let term = document.getElementById('searchField').value
+    let searchTerm = document.getElementById('searchField').value
 
-    console.log(term)
+    if (searchTerm) {
+        searchWeather(searchTerm)
+    }
 })
